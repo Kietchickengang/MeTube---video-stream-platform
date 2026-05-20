b6e75a4e182ccad8348bf0dc301a30e96ce53d85
Kietchickengang <Kietboy53@gmail.com>
2026-05-20 23:20:31 +0700

    Save for sure v1

--- frontend/Metube-UI/src/page/LayOut.jsx
+++ frontend/Metube-UI/src/page/LayOut.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Navbar from "../components/NavigationBar";
@@ -11,27 +11,30 @@ import UploadPage from "./UploadPage";
 const LayOut = () => {
   const location = useLocation();
   const isVideoPage = location.pathname.startsWith("/video/");
-  // Home mặc định mở sidebar
+  // Home page default open sidebar
   const [sidebarOpen, setSidebarOpen] = useState(true);
   const [showUploadPage, setShowUploadPage] = useState(false);
 
+  // Hide sidebar when click video
+  useEffect(() => { 
+    if(isVideoPage) { 
+      setSidebarOpen(false); 
+    } 
+  }, [isVideoPage]);
+
   return (
     <div className="bg-[#0f0f0f] min-h-screen text-[#f1f1f1]">
-
       <Navbar
         goToUploadPage={() => setShowUploadPage(true)}
         toggleSidebar={() => setSidebarOpen(v => !v)}
       />
-
       <div className="flex pt-14">
-
         {/* ===== HOMEPAGE SIDEBAR ===== */}
         {!isVideoPage && sidebarOpen && (
           <div className="shrink-0">
             <Sidebar />
           </div>
         )}
-
         {/* ===== VIDEO PAGE OVERLAY SIDEBAR ===== */}
         {isVideoPage && sidebarOpen && (
           <>
@@ -40,7 +43,6 @@ const LayOut = () => {
               className="fixed inset-0 bg-black/50 z-40"
               onClick={() => setSidebarOpen(false)}
             />
-
             {/* OVERLAY SIDEBAR */}
             <div className="fixed top-14 left-0 z-50">
               <Sidebar />
@@ -54,7 +56,6 @@ const LayOut = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/video/:id" element={<VideoPage />} />
             <Route path="/upload" element={<UploadPage />} />
-
             <Route path="/shorts" element={<div>Shorts Page</div>} />
             <Route path="/history" element={<div>History Page</div>} />
             <Route path="/myVid" element={<div>My Videos</div>} />
