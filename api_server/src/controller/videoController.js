37960d3d3bb83e7570108eda2bde065d2c6f8ea5
LocK48 <321quangloc@gmail.com>
2026-05-20 20:07:11 +0700

    added real view count and user name

--- api_server/src/controller/videoController.js
+++ api_server/src/controller/videoController.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { VIDEO_STATUS } from "../util/constants.js";
-import { getPresignedURL } from '../util/presignedURL.js';
+import { getPresignedURL } from "../util/presignedURL.js";
 import { checkHeadRequestS3 } from "../middleware/validate.js";
 import { VideoDB_operation } from "../../../worker_server/src/service/db.js";
 import { vnTimeString } from "../util/helper.js";
@@ -10,207 +10,224 @@ import { addJobToQueue } from "../service/queue.js";
 
 const raw_video_bucket = process.env.BUCKET_RAW_VIDEO;
 const processed_video_bucket = process.env.BUCKET_PROCESSED_VIDEO;
-const secret_key = process.env.AES_SECRET_KEY; 
+const secret_key = process.env.AES_SECRET_KEY;
 
-const { updateStatus, updateByVideoId, findByVideoId, create, findAll } = VideoDB_operation;
+const { updateStatus, updateByVideoId, findByVideoId, create, findAll } =
+  VideoDB_operation;
 
-export const generatePresignedURL = async(req, res) => {
-    try{
-        const { fileName, contentType, fileSize } = req.body;
-        const { url, fields, videoId } = await getPresignedURL({
-            fileName: fileName, 
-            bucket: raw_video_bucket,
-            contentType: contentType,
-            fileSize: fileSize,
-        });
+export const generatePresignedURL = async (req, res) => {
+  try {
+    const { fileName, contentType, fileSize } = req.body;
+    const { url, fields, videoId } = await getPresignedURL({
+      fileName: fileName,
+      bucket: raw_video_bucket,
+      contentType: contentType,
+      fileSize: fileSize,
+    });
 
-        return res.status(200).json({
-            url: url,
-            key: videoId,
-            fields: fields,
-        })
-    }
-    catch(err){
-        console.log(`Something wrong with presigned URL service: ${err.message}`);
-        res.status(500).json({
-            message: " Failed to generate presigned URL for object upload",
-            error: err.message,
-        })
-    }
-}
+    return res.status(200).json({
+      url: url,
+      key: videoId,
+      fields: fields,
+    });
+  } catch (err) {
+    console.log(`Something wrong with presigned URL service: ${err.message}`);
+    res.status(500).json({
+      message: " Failed to generate presigned URL for object upload",
+      error: err.message,
+    });
+  }
+};
 
-export const confirmUpload = async(req, res) => {
-    try{
-        // Extract videoId from client request
-        const { videoId } = req.params;
-        const decryptVideoId = decrypting(secret_key, videoId);
+export const confirmUpload = async (req, res) => {
+  try {
+    // Extract videoId from client request
+    const { videoId } = req.params;
+    const decryptVideoId = decrypting(secret_key, videoId);
 
-        // Check if video file is available in Vietnix
-        const fileIsExist = await checkHeadRequestS3(raw_video_bucket, decryptVideoId);
-        if(!fileIsExist) return res.status(400).json({
-            message: "Video is not existed",
-        })
-        return res.status(200).json({
-            message: "Valid video, upload confirmation successfully",
-            data: {
-                status: VIDEO_STATUS.PROCESSING,
-            },
-            time: vnTimeString(),
-        })
-    }
-    catch(err){
-        console.log(`Upload confirmation failed: ${err.message}`);
-        return res.status(500).json({
-            message: "Upload confirmation failed.Try again",
-            error: err.message,
-        })
-    }
-}
+    // Check if video file is available in Vietnix
+    const fileIsExist = await checkHeadRequestS3(
+      raw_video_bucket,
+      decryptVideoId,
+    );
+    if (!fileIsExist)
+      return res.status(400).json({
+        message: "Video is not existed",
+      });
+    return res.status(200).json({
+      message: "Valid video, upload confirmation successfully",
+      data: {
+        status: VIDEO_STATUS.PROCESSING,
+      },
+      time: vnTimeString(),
+    });
+  } catch (err) {
+    console.log(`Upload confirmation failed: ${err.message}`);
+    return res.status(500).json({
+      message: "Upload confirmation failed.Try again",
+      error: err.message,
+    });
+  }
+};
 
-export const checkStatusUpload = async(req, res) => {
-    try{
-        const { videoId } = req.params;
-        const uploadVideo = await findByVideoId(videoId);
-        return res.status(200).json({
-            message: "Accessed information from Database successfully",
-            data: {
-                uploadStatus: uploadVideo.status,
-            },
-            time: vnTimeString(),
-        })
-    }
-    catch(err){
-        console.log(`Can not get information from Database: ${err}`);
-        return res.status(500).json({
-            message: "Get information failed.Try again",
-            error: err.message,
-        })
-    }
-}
+export const checkStatusUpload = async (req, res) => {
+  try {
+    const { videoId } = req.params;
+    const uploadVideo = await findByVideoId(videoId);
+    return res.status(200).json({
+      message: "Accessed information from Database successfully",
+      data: {
+        uploadStatus: uploadVideo.status,
+      },
+      time: vnTimeString(),
+    });
+  } catch (err) {
+    console.log(`Can not get information from Database: ${err}`);
+    return res.status(500).json({
+      message: "Get information failed.Try again",
+      error: err.message,
+    });
+  }
+};
 
-export const updateSubmitDB = async(req, res) => {
-    try{
-        const { videoId } = req.params;
-        const data = req.body;
+export const updateSubmitDB = async (req, res) => {
+  try {
+    const { videoId } = req.params;
+    const data = req.body;
 
-        await updateByVideoId(videoId, data);
+    await updateByVideoId(videoId, data);
 
-        return res.status(200).json({
-            message: "Updated processing status for video successfully",
-            time: vnTimeString(),
-        }) 
-    }
-    catch(err){
-        console.log(`Can not update processing status for video: ${err}`);
-        return res.status(500).json({
-            message: "Update processing status failed.Try again",
-            error: err.message,
-        })
-    }
-}
+    return res.status(200).json({
+      message: "Updated processing status for video successfully",
+      time: vnTimeString(),
+    });
+  } catch (err) {
+    console.log(`Can not update processing status for video: ${err}`);
+    return res.status(500).json({
+      message: "Update processing status failed.Try again",
+      error: err.message,
+    });
+  }
+};
 
-export const initStatusDB = async(req, res) => {
-    try{
-        const { videoId } = req.params;
-        const { videoPath, videoSize, mimeType } = req.body;
+export const initStatusDB = async (req, res) => {
+  try {
+    const { videoId } = req.params;
+    const { videoPath, videoSize, mimeType } = req.body;
 
-        // Hash videoId from vietnix by default
-        await create(standardInputDB({
-            videoId: videoId,
-            videoPath: videoPath,
-            videoSize: videoSize,
-            mimeType: mimeType,
-        }));
-        return res.status(200).json({
-            message: "Initialized DB successfully",
-            time: vnTimeString(),
-        })
-    }
-    catch(err){
-        console.log(`Can not initialize DB for video: ${err}`);
-        return res.status(500).json({
-            message: "Initialized DB failed.Try again",
-            error: err.message,
-        })
-    }
-}
+    // Hash videoId from vietnix by default
+    await create(
+      standardInputDB({
+        videoId: videoId,
+        videoPath: videoPath,
+        videoSize: videoSize,
+        mimeType: mimeType,
+        userId: req.user.id,
+      }),
+    );
+    return res.status(200).json({
+      message: "Initialized DB successfully",
+      time: vnTimeString(),
+    });
+  } catch (err) {
+    console.log(`Can not initialize DB for video: ${err}`);
+    return res.status(500).json({
+      message: "Initialized DB failed.Try again",
+      error: err.message,
+    });
+  }
+};
 
-export const uploadThumbS3 = async(req, res) => {
-    try{
-        const { fileName, contentType, fileSize, folderName } = req.body;
-        const { url, fields, videoId } = await getPresignedURL({
-            fileName: fileName, 
-            folderName: folderName,
-            bucket: processed_video_bucket,
-            contentType: contentType,
-            fileSize: fileSize,
-            nameDir: "thumbnail.jpg",
-        });
+export const uploadThumbS3 = async (req, res) => {
+  try {
+    const { fileName, contentType, fileSize, folderName } = req.body;
+    const { url, fields, videoId } = await getPresignedURL({
+      fileName: fileName,
+      folderName: folderName,
+      bucket: processed_video_bucket,
+      contentType: contentType,
+      fileSize: fileSize,
+      nameDir: "thumbnail.jpg",
+    });
 
-        return res.status(200).json({
-            url: url,
-            key: videoId,
-            fields: fields,
-        })
-    }
-    catch(err){
-        console.log(`Can not upload user file to S3: ${err.message}`);
-        res.status(500).json({
-            message: " Failed to upload user file to vietnix",
-            error: err.message,
-        })
-    }
-}
+    return res.status(200).json({
+      url: url,
+      key: videoId,
+      fields: fields,
+    });
+  } catch (err) {
+    console.log(`Can not upload user file to S3: ${err.message}`);
+    res.status(500).json({
+      message: " Failed to upload user file to vietnix",
+      error: err.message,
+    });
+  }
+};
 
-export const callWorker = async(req, res) => {
-    try{
-        const { videoId } = req.params;
-        const { timestamp, file } = req.body;
-        // Look up video in DB
-        const video = await findByVideoId(videoId);
-        // Enqueue job for worker to handle
-        await addJobToQueue({
-            videoId,
-            videoPath: video.videoPath,
-            timestamp: timestamp,
-            file: file,
-        })
-        return res.status(200).json({
-            message: "Pushed job successfully",
-            time: vnTimeString(),
-        })
-    }
-    catch(err){
-        console.log(`Can not connect to worker service: ${err}`);
-        return res.status(500).json({
-            message: "Call worker service failed.Try again",
-            error: err.message,
-        })
-    }
-}
+export const callWorker = async (req, res) => {
+  try {
+    const { videoId } = req.params;
+    const { timestamp, file } = req.body;
+    // Look up video in DB
+    const video = await findByVideoId(videoId);
+    // Enqueue job for worker to handle
+    await addJobToQueue({
+      videoId,
+      videoPath: video.videoPath,
+      timestamp: timestamp,
+      file: file,
+    });
+    return res.status(200).json({
+      message: "Pushed job successfully",
+      time: vnTimeString(),
+    });
+  } catch (err) {
+    console.log(`Can not connect to worker service: ${err}`);
+    return res.status(500).json({
+      message: "Call worker service failed.Try again",
+      error: err.message,
+    });
+  }
+};
 
-export const getAllVideos = async(req, res) => {
-    try{
-        const allVideos = await VideoDB_operation.findAll();
-        res.status(200).json(allVideos);
-    }
-    catch(err){
-        console.error("Error in getting videos:", err);
-        res.status(500).json({ message: "Can not get videos list" });
-    }
-}
+export const getAllVideos = async (req, res) => {
+  try {
+    const allVideos = await VideoDB_operation.findAll();
 
-export const getVideoById = async(req, res) => {
-    try{
-        const { videoId } = req.params;
-        const video = await findByVideoId(videoId);
-        if (!video) {
-            return res.status(404).json({ message: "Can not find video" });
-        }
-        res.status(200).json(video);
-    }   
-    catch(err){
-        console.error("Error in getting selected video:", err);
-        res.status(500).json({ message: "Can not get video" });
+    res.status(200).json(allVideos);
+  } catch (err) {
+    console.error("Error in getting videos:", err);
+
+    res.status(500).json({
+      message: "Can not get videos list",
+    });
+  }
+};
+
+export const getVideoById = async (req, res) => {
+  try {
+    const { videoId } = req.params;
+
+    const video = await findByVideoId(videoId);
+
+    if (!video) {
+      return res.status(404).json({
+        message: "Can not find video",
+      });
     }
-}
+
+    await updateByVideoId(videoId, {
+      $inc: { views: 1 },
+    });
+
+    const updatedVideo = await findByVideoId(videoId);
+
+    res.status(200).json(updatedVideo);
+  } catch (err) {
+    console.error("Error in getting selected video:", err);
+
+    res.status(500).json({
+      message: "Can not get video",
+    });
+  }
+};
--- api_server/src/model/videoModel.js
+++ api_server/src/model/videoModel.js
@@ -2,29 +2,46 @@ import { ObjectId } from "mongodb";
 import { VIDEO_STATUS } from "../util/constants.js";
 
 export const standardInputDB = (data) => {
-    const now = new Date();
+  const now = new Date();
 
-    const { videoId, userId, title, description, videoPath, duration, resolution, videoSize, mimeType } = data;
+  const {
+    videoId,
+    userId,
+    title,
+    description,
+    videoPath,
+    duration,
+    resolution,
+    videoSize,
+    mimeType,
+  } = data;
 
-    return {
-        videoId: videoId,
-        userId: new ObjectId(userId),
-        title: title || "",
+  return {
+    videoId: videoId,
+    userId: userId ? new ObjectId(userId) : null,
 
-        description: description || "",
-        status: VIDEO_STATUS.UPLOADING,
-        videoPath: videoPath || "",
-        hlsPath: "",
-        thumbnailUrl: "",
-        duration: duration || 0,
-        resolution: resolution || [],
-        videoSize: videoSize || 0,
-        mimeType: mimeType || "",
+    title: title || "",
+    description: description || "",
 
-        unexpected_err: "",
-        retryCnt: 0,
+    status: VIDEO_STATUS.UPLOADING,
 
-        createdAt: now,
-        updatedAt: now,
-    };
+    videoPath: videoPath || "",
+    hlsPath: "",
+    thumbnailUrl: "",
+
+    duration: duration || 0,
+    resolution: resolution || [],
+
+    videoSize: videoSize || 0,
+    mimeType: mimeType || "",
+
+    // FIX HERE
+    views: 0,
+
+    unexpected_err: "",
+    retryCnt: 0,
+
+    createdAt: now,
+    updatedAt: now,
+  };
 };
